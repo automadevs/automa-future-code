a82b4081ee6de6c0e9f427ef6974d3bb3c56aabb
automa <136636118+automadevs@users.noreply.github.com>
2025-10-19 11:39:42 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "automa-future-code",
+  base: "/automa-future-code/",
   server: {
     host: "::",
     port: 8080,
